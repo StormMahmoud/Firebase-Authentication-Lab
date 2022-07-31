cf09b55ad0f29d73bd9a8746f0adaf755edcd7fd
StormMahmoud <39629523+StormMahmoud@users.noreply.github.com>
2022-07-31 18:46:08 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -5,14 +5,45 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+config = {
+ apiKey: "AIzaSyAHi89SznT5b_FgOoW4T8BCK1nULEP8uhM",
+  authDomain: "mahmoud-80a43.firebaseapp.com",
+  projectId: "mahmoud-80a43",
+  storageBucket: "mahmoud-80a43.appspot.com",
+  messagingSenderId: "780139978483",
+  appId: "1:780139978483:web:94cfe6528fa449957ee186",
+  measurementId: "G-GW2TRJ0672"
+  }
+
+firebase = pyrebase.initialia_app(config)
+auth = firebase.auth()
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    erorr = ""
+    if request.form = 'POST':
+        email = request.form['email']
+        password =  request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+
+    return render_template("signup.html")
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    erorr = ""
+    if request.form = 'POST':
+        email = request.form['email']
+        password =  request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+
     return render_template("signup.html")
 
 
