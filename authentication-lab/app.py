73a1e6bac1cd0e2cf05cdfc614488135eb2d7cf7
StormMahmoud <39629523+StormMahmoud@users.noreply.github.com>
2022-08-01 14:12:20 +0300

    .

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -6,45 +6,51 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 config = {
- apiKey: "AIzaSyAHi89SznT5b_FgOoW4T8BCK1nULEP8uhM",
-  authDomain: "mahmoud-80a43.firebaseapp.com",
-  projectId: "mahmoud-80a43",
-  storageBucket: "mahmoud-80a43.appspot.com",
-  messagingSenderId: "780139978483",
-  appId: "1:780139978483:web:94cfe6528fa449957ee186",
-  measurementId: "G-GW2TRJ0672"
-  }
+  "apiKey": "AIzaSyAHi89SznT5b_FgOoW4T8BCK1nULEP8uhM",
+  "authDomain": "mahmoud-80a43.firebaseapp.com",
+  "projectId": "mahmoud-80a43",
+  "storageBucket": "mahmoud-80a43.appspot.com",
+  "messagingSenderId": "780139978483",
+  "appId": "1:780139978483:web:94cfe6528fa449957ee186",
+  "measurementId": "G-GW2TRJ0672",
+  "databaseURL" :"" }
+  
 
-firebase = pyrebase.initialia_app(config)
+firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
 
+#ry:
+            
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    erorr = ""
-    if request.form = 'POST':
+    if request.method == 'POST':
+        print("heyy")
         email = request.form['email']
-        password =  request.form['password']
+        password = request.form['password']
         try:
             login_session['user'] = auth.sign_in_with_email_and_password(email,password)
             return redirect(url_for('add_tweet'))
+        #except:
+         #   return render_template('signin.html')
+    else:
+        return render_template('signin.html')
 
-    return render_template("signup.html")
-    return render_template("signin.html")
-
+    
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     erorr = ""
-    if request.form = 'POST':
+    if request.method == 'POST':
         email = request.form['email']
         password =  request.form['password']
-        try:
-            login_session['user'] = auth.create_user_with_email_and_password(email,password)
-            return redirect(url_for('add_tweet'))
-
-    return render_template("signup.html")
+        #try:
+        login_session['user'] = auth.create_user_with_email_and_password(email,password)
+        return redirect(url_for('signin'))
+        #except:
+         #   return render_template("signup.html")
+    return render_template("signup.html")        
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
