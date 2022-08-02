4580afc4cf6bf9a312c82a1aa64e9bf8f5aadb79
StormMahmoud <39629523+StormMahmoud@users.noreply.github.com>
2022-08-02 03:36:57 +0300

    .

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -13,15 +13,12 @@ config = {
   "messagingSenderId": "780139978483",
   "appId": "1:780139978483:web:94cfe6528fa449957ee186",
   "measurementId": "G-GW2TRJ0672",
-  "databaseURL" :"" }
-  
+  "databaseURL" :"https://mahmoud-80a43-default-rtdb.europe-west1.firebasedatabase.app/" 
+  }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
-
-
-#ry:
-            
+db = firebase.database()            
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
@@ -29,24 +26,26 @@ def signin():
         print("heyy")
         email = request.form['email']
         password = request.form['password']
-        try:
-            login_session['user'] = auth.sign_in_with_email_and_password(email,password)
-            return redirect(url_for('add_tweet'))
-        #except:
-         #   return render_template('signin.html')
+        login_session['user'] = auth.sign_in_with_email_and_password(email,password)
+        return redirect(url_for('add_tweet'))
     else:
         return render_template('signin.html')
 
-    
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    erorr = ""
+    
     if request.method == 'POST':
         email = request.form['email']
         password =  request.form['password']
-        #try:
-        login_session['user'] = auth.create_user_with_email_and_password(email,password)
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email,password)
+            user = {"full_name": request.form('full_name'), 
+            db.child("Users").child(login_session['user']['localId']).set(user)
+            "username": request.form('username'), "bio": request.form('bio')
+            }
+        except:
+            return render_template("signup.html")
+           
         return redirect(url_for('signin'))
         #except:
          #   return render_template("signup.html")
@@ -55,7 +54,15 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':  
+        tweet = {"uid": db.child("Users").child(login_session['user']['localId']).get().val()}
+        tweets = {"title": request.form['title'], "text": request.form['text']}
+        db.child('Tweets').push(tweets)
     return render_template("add_tweet.html")
+
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def add_tweet(x):
+    return render_template("add_tweet.html", x = db.child("Tweets").child(login_session['user']['localId']).get().val()   
 
 
 if __name__ == '__main__':
